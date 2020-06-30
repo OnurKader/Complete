f1d90065ac49c692131d738f052a399f7e2b03f4
OnurKader <onurkader@protonmail.com>
2020-06-30 19:39:13 +0300

    Test: Added insertion and reading from file
    > Checks the length of the Trie for now

--- src/Trie_test.cpp
+++ src/Trie_test.cpp
@@ -1,9 +1,30 @@
+#include "Timer.hpp"
 #include "Trie.hpp"
 
 #include <fmt/format.hpp>
+#include <fstream>
 
 int main()
 {
-	fmt::print("Hello There!\n");
+	OK::Trie trie;
+	std::ifstream dictionary_file {"../data/dictionary.txt"};
+	if(!dictionary_file)
+	{
+		fmt::print("Couldn't open dictionary file '../data/dictionary.txt'\n");
+		return 1;
+	}
+
+	std::string dictionary_word;
+	{
+		Timer trie_timer {"Trie inserting 58109 words"};
+		// Takes ~34ms
+		while(dictionary_file >> dictionary_word)
+		{
+			trie.push(dictionary_word);
+		}
+	}
+
+	massert(trie.word_count() == 58109ULL, "Couldn't insert all the words");
+
 	return 0;
 }
